a07b9c92abadaaf4e546aa0897c1d75d19d42f07
PaulaBSBCAT <paula.ribeiro@estudante.ifgoiano.edu.br>
2023-04-12 20:20:43 -0300

    listafavoritos

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -8,7 +9,6 @@ void main() {
 
 class MyApp extends StatelessWidget { //caracteristicas que nao mudam
   const MyApp({super.key});
-
   
   @override
   Widget build(BuildContext context) {
@@ -26,7 +26,6 @@ class MyApp extends StatelessWidget { //caracteristicas que nao mudam
     );
   }
 }
-
 
 class MyAppState extends ChangeNotifier{
   var current = WordPair.random();
@@ -48,39 +47,63 @@ class MyAppState extends ChangeNotifier{
   }
 }
 
-class MyHomePage extends StatelessWidget{
+class MyHomePage extends StatefulWidget{
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
   @override
   Widget build(BuildContext context){
-    return Scaffold(
-      body: Row(
-        children: [
-          SafeArea(
-            child: NavigationRail(
-              extended: false,
-              destinations: [
-                NavigationRailDestination(
-                  icon: Icon(Icons.home),
-                  label: Text('Home'),
+    Widget page;
+    switch(selectedIndex){
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = FavoritePage();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    return LayoutBuilder(
+      builder: (context,constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              SafeArea(
+                child: NavigationRail(
+                  extended: constraints.maxWidth >= 600,
+                  destinations: [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.home),
+                      label: Text('Home'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.favorite),
+                      label: Text('Favorites'),
+                    ),
+                  ],
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: (value){
+                    setState(() {
+                      selectedIndex=value;
+                    });
+                  },
                 ),
-                NavigationRailDestination(
-                  icon: Icon(Icons.favorite),
-                  label: Text('Favorites'),
+              ),
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: page,
                 ),
-              ],
-              selectedIndex: 0,
-              onDestinationSelected: (value){
-                print('selected:$value');
-              },
-            ),
+              ),
+            ],
           ),
-          Expanded(
-            child: Container(
-              color: Theme.of(context).colorScheme.primaryContainer,
-              child: GeneratorPage(),
-            ),
-          ),
-        ],
-      ),
+        );
+      }
     );
   }
 }
@@ -154,3 +177,29 @@ class BigCard extends StatelessWidget {
     );
   }
 }
+
+class FavoritePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+
+    var appState = context.watch<MyAppState>();
+    if (appState.favorites.isEmpty){
+      return Center(child: Text('no favorites yet'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding:const EdgeInsets.all(20),
+          child: Text('you have ${appState.favorites.length} favorites: '),
+          ),
+          for(var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
+    );
+  } // build
+} // classe
